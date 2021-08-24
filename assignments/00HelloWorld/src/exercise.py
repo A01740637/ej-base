fc8526887dbfbbb64395651edb9c6b5d07826bad
casandraortiz <A01740637@itesm.mx>
2021-08-24 13:51:02 +0000

    act 10. ejercicio 1

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,13 @@
+def equivalente(horas, minutos, segundos):
+    minutosAsegundos = minutos * 60
+    horasAsegundos = horas * 3600
+    totalsegundos = segundos + minutosAsegundos + horasAsegundos
+    return totalsegundos
+
 def main():
     #escribe tu código abajo de esta línea
     pass
+    print(equivalente(2,20,8))
 
 if __name__=='__main__':
     main()
